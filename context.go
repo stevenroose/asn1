f5f6037a6b0544e87bd34f6421a247469aca95ee
Marcelo Cerri <mhcerri@gmail.com>
2016-03-02 12:51:36 -0300

    Fix example for AddChoice in its Go doc.

--- context.go
+++ context.go
@@ -128,7 +128,7 @@ func (this *Context) addChoiceEntry(choice string, entry choiceEntry) error {
 //	}
 //	// A Context with the registered choices
 //	ctx := asn1.NewContext()
-//	ctx.AddChoice("value", []Choice {
+//	ctx.AddChoice("value", []asn1.Choice {
 //		{
 //			Type: reflect.TypeOf(int(0)),
 //		},
