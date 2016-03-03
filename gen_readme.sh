a63da7cd9a2774146514f4f0101dcf1ff35dc859
Marcelo Cerri <mhcerri@gmail.com>
2016-03-03 17:43:20 -0300

    Add GoDoc badge

--- gen_readme.sh
+++ gen_readme.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Add Travis badge:
+# Add badges:
 cat > ./README.md << 'EOF'
-[![Build Status](https://travis-ci.org/PromonLogicalis/asn1.svg?branch=master)](https://travis-ci.org/PromonLogicalis/asn1)
+[![Build Status](https://travis-ci.org/PromonLogicalis/asn1.svg?branch=master)](https://travis-ci.org/PromonLogicalis/asn1) [![GoDoc](https://godoc.org/github.com/PromonLogicalis/asn1?status.svg)](https://godoc.org/github.com/PromonLogicalis/asn1)
 EOF
 
 # Add Go doc
